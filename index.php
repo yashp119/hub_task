ee672b68ab1ee00ab18f18631e784a7008d0fb2c
yashp119 <125194772+yashp119@users.noreply.github.com>
2023-08-01 15:27:41 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP App</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is my PHP application deployed on a LAMP stack!</p>
+</body>
+</html>
